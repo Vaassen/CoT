393fdd9f92530b506a77f57c59b92793d68d758a
badook <alalattack@gmail.com>
2024-06-02 19:59:15 +0100

    Min relations -25

--- common/defines/cot_defines.lua
+++ common/defines/cot_defines.lua
@@ -4,3 +4,5 @@ NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35
 NDefines.Ncountry.LAND_FORCELIMIT_EXTRA_COST_FACTOR = 3
 NDefines.NCountry.NAVAL_FORCELIMIT_EXTRA_COST_FACTOR = 6
 NDefines.NEconomy.MIN_ARMY_MAINTENANCE = 0.20
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -25,						-- Alliances not possible if either country has an opinion of the other lower than this 
+
