45d6348d699a881b428fe19cb541d8ea82f06910
Bonbon <meganstrenner@gmail.com>
2025-04-24 06:12:57 -0700

    liches unified

--- magictextgen.sh
+++ magictextgen.sh
@@ -1,20 +1,7 @@
 for school in abjuration conjuration divination enchantment evocation illusion necromancy transmutation
 do
 schoolUpper="$(tr '[:lower:]' '[:upper:]' <<< ${school:0:1})${school:1}" #Mac doesn't have Bash 4
-echo "frame = {" >> temp.txt
-echo "			number = 4" >> temp.txt
-echo "			trigger = { is_overall_${school}_level_3 = yes }" >> temp.txt
-echo "		}" >> temp.txt
-echo "		frame = {" >> temp.txt
-echo "			number = 3" >> temp.txt
-echo "			trigger = { is_overall_${school}_level_1 = yes }" >> temp.txt
-echo "		}" >> temp.txt
-echo "		frame = {" >> temp.txt
-echo "			number = 2" >> temp.txt
-echo "			trigger = { is_overall_${school}_level_2 = yes }" >> temp.txt
-echo "		}" >> temp.txt
-echo "		frame = { number = 1 }" >> temp.txt
-echo "" >> temp.txt
+echo " ${school}_advancement:0 "Advancing §Y${schoolUpper}§!"" >> temp.txt
 done
 
 #Delete after Magic Rework is done!
