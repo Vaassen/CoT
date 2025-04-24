8afa8910b5d25339413c267f5b9ad422ea19503e
Bonbon <meganstrenner@gmail.com>
2025-04-24 02:38:29 -0700

    frame and tooltip fix

--- magictextgen.sh
+++ magictextgen.sh
@@ -1,7 +1,20 @@
 for school in abjuration conjuration divination enchantment evocation illusion necromancy transmutation
 do
 schoolUpper="$(tr '[:lower:]' '[:upper:]' <<< ${school:0:1})${school:1}" #Mac doesn't have Bash 4
-
+echo "frame = {" >> temp.txt
+echo "			number = 4" >> temp.txt
+echo "			trigger = { is_overall_${school}_level_3 = yes }" >> temp.txt
+echo "		}" >> temp.txt
+echo "		frame = {" >> temp.txt
+echo "			number = 3" >> temp.txt
+echo "			trigger = { is_overall_${school}_level_1 = yes }" >> temp.txt
+echo "		}" >> temp.txt
+echo "		frame = {" >> temp.txt
+echo "			number = 2" >> temp.txt
+echo "			trigger = { is_overall_${school}_level_2 = yes }" >> temp.txt
+echo "		}" >> temp.txt
+echo "		frame = { number = 1 }" >> temp.txt
+echo "" >> temp.txt
 done
 
 #Delete after Magic Rework is done!
