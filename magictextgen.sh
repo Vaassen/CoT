24e96c46e436476b6aaf22fc91cdc07c5ef590a7
Bonbon <meganstrenner@gmail.com>
2025-04-25 22:19:28 -0700

    more lichdom things, general hierarchy improvement

--- magictextgen.sh
+++ magictextgen.sh
@@ -1,7 +1,7 @@
 for school in abjuration conjuration divination enchantment evocation illusion necromancy transmutation
 do
 schoolUpper="$(tr '[:lower:]' '[:upper:]' <<< ${school:0:1})${school:1}" #Mac doesn't have Bash 4
-echo " ${school}_advancement:0 "Advancing §Y${schoolUpper}§!"" >> temp.txt
+echo "	[[${school}]set_ruler_flag = ${school}_\$${school}\$]" >> temp.txt
 done
 
 #Delete after Magic Rework is done!
