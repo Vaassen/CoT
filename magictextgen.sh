d7226b700e6f4729fbafd0355627844e55dfa7e3
Bonbon <meganstrenner@gmail.com>
2025-04-26 16:47:18 -0700

    magic project unification and homunculi update

--- magictextgen.sh
+++ magictextgen.sh
@@ -1,7 +1,10 @@
 for school in abjuration conjuration divination enchantment evocation illusion necromancy transmutation
 do
 schoolUpper="$(tr '[:lower:]' '[:upper:]' <<< ${school:0:1})${school:1}" #Mac doesn't have Bash 4
-echo "	[[${school}]set_ruler_flag = ${school}_\$${school}\$]" >> temp.txt
+done
+
+for project in magical_fortress summon_extraplanars orb_of_omniscience enchant_legendary_artifact battlemage_academy theatre_of_simulacra lichdom homunculus unique_project_1 unique_project_2 unique_project_3 unique_project_4 unique_project_5 unique_project_6 unique_project_7 unique_project_8
+do
 done
 
 #Delete after Magic Rework is done!
